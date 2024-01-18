9aacd54e5ce75881886f5c7cee7c22f4ddb7dec0
CHOGYEONGMOON <aqazswsx@etribe.co.kr>
2024-01-18 11:41:18 +0900

    token transformer 추가(global.json)

---
+++ styleDictionaryConfig.js
@@ -0,0 +1,16 @@
+const StyleDictionary = require('style-dictionary').extend({
+  source: ['./global.json'],
+  platforms: {
+    scss: {
+      transformGroup: 'scss',
+      buildPath: '',
+      files: [{
+        destination: 'variables.scss',
+        format: 'scss/variables'
+      }]
+    }
+    // ...
+  }
+});
+
+StyleDictionary.buildAllPlatforms();
